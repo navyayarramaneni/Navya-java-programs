fde6328a9639d03a2286e9abfba53cbf6aa2bfe9
navyayarramaneni <yarramaneninavya@gmail.com>
2025-08-14 10:40:36 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,15 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        String input = "madam";
+        String reversed = "";
+        input = input.toLowerCase();
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+        if (input.equals(reversed)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+}
